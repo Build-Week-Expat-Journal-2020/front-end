f052908feee54fa4dcef66bec5c087e25fdf132d
Mali Rivera <mali@Malis-MacBook-Pro.local>
2020-11-16 15:57:14 -0500

    posts action & reducer

---
+++ client/src/reducers/postsReducer.js
@@ -0,0 +1,17 @@
+import { GET_POSTS } from '../actions/postsActions'
+
+const initialState = {
+    posts = []
+}
+
+export const postsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_POSTS: 
+            return {
+                ...state, 
+                posts: action.paylod
+            }
+        default: 
+            return state
+    }
+}
